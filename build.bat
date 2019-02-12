c8397bb12def419f65386f14064b990da300b1a6
Jackson Moore <jacksonsamuelmoore@gmail.com>
2019-02-12 06:39:00 +0000

    Fix trailing slash and brief cleanup

--- build.bat
+++
@@ -1,9 +0,0 @@
-cd public/booking
-CMD /c polymer build
-cd ../mangocast
-CMD /c polymer build
-cd ../../
-git add --all
-git commit -m "Build %DATE% %TIME%"
-git push origin master
-CMD /c firebase deploy
