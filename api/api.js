db095302365bcdd600b0daababf124623aa9e5ef
Jackson Moore <jacksonsamuelmoore@gmail.com>
2019-02-14 11:58:06 +0000

    Try logging with nginx

--- api/api.js
+++ api/api.js
@@ -743,7 +743,6 @@ api.post("/api/booking/mail", (request, response) => {
     }
 })
 api.get("**", (request, response) => {
-    console.log(request.path, request.method)
     response.send(403, "<h1>Forbidden</h1><p>You can't access this sector, and to be honest, that probably means there's nothing here. It's best if you went somewhere else please!</p></br>" + request.path)
 })
 api.listen(3000, () => console.log(`Mail app listening on port 3000`))
