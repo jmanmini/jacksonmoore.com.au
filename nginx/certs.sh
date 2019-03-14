b9b4e00b3b81b11795aaf12c938f5f548f22d0a1
Jackson Moore <jacksonsamuelmoore@gmail.com>
2019-03-14 23:01:12 +0000

    Datadog upgrades

--- nginx/certs.sh
+++ nginx/certs.sh
@@ -1,1 +1,1 @@
-sudo certbot certonly --manual --preferred-challenges=dns --server https://acme-v02.api.letsencrypt.org/directory --email jackson@ckjom.com -d *.jacksonmoore.com.au
+sudo certbot certonly --manual --preferred-challenges=dns --server https://acme-v02.api.letsencrypt.org/directory --email jackson@ckjom.com -d *.jacksonmoore.com.au -d jacksonmoore.com.au
