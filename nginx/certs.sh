0b497b003a315eb7e5a342eebdd58f6f96846dd5
Jackson Moore <jacksonsamuelmoore@gmail.com>
2019-10-29 10:43:30 +0000

    Update certs

--- nginx/certs.sh
+++ nginx/certs.sh
@@ -1,1 +1,4 @@
 sudo certbot certonly --manual --preferred-challenges=dns --server https://acme-v02.api.letsencrypt.org/directory --email jackson@ckjom.com -d *.jacksonmoore.com.au -d jacksonmoore.com.au
+
+
+C:\Users\jacks\AppData\Local\Packages\CanonicalGroupLimited.UbuntuonWindows_79rhkp1fndgsc\LocalState\rootfs\etc\letsencrypt\archive
